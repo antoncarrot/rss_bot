e68934063157fca51e7f74413594031470c93bd1
antoncarrot <antoncarrot@users.noreply.github.com>
2022-05-04 16:13:47 +0300

    #1 Update config

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import os
 
+TG_API_TOKEN = os.getenv("TG_API_TOKEN")
+
+DEBUG = True if os.getenv("DEBUG") else False
+
 LOG_FORMAT = "%(asctime)s %(levelname)s %(name)s.%(funcName)s:%(lineno)s %(message)s"
 
 LOGGING = {
@@ -8,7 +12,7 @@ LOGGING = {
     "formatters": {"verbose": {"format": LOG_FORMAT}},
     "handlers": {"console": {"class": "logging.StreamHandler", "formatter": "verbose"}},
     "root": {
-        "level": "DEBUG" if os.getenv("DEBUG") else "INFO",
+        "level": "DEBUG" if DEBUG else "INFO",
         "handlers": ["console"],
     },
 }
