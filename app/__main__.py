1dbbe5d3d40b13e86cf62ec48f9064bd728a1c12
antoncarrot <antoncarrot@users.noreply.github.com>
2022-05-05 11:59:38 +0300

    #1 Add tg handlers

--- app/__main__.py
+++ app/__main__.py
@@ -1,11 +1,20 @@
 import asyncio
 import logging
 
+from .tg_bot import init_tg_bot
+
 logger = logging.getLogger(__name__)
 
 
 async def main():
+    dp = await init_tg_bot()
     logger.info("Starting bot")
+    try:
+        await dp.start_polling()
+    finally:
+        await dp.storage.close()
+        await dp.storage.wait_closed()
+        await dp.bot.get_session().close()
 
 
 asyncio.run(main())
