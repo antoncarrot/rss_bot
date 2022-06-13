c8e8811d6e9ac1c73292a59be4be379efccb35fe
antoncarrot <antoncarrot@users.noreply.github.com>
2022-06-13 21:56:45 +0300

    #5 Add start handler

--- app/tg_bot/handlers/start.py
+++ app/tg_bot/handlers/start.py
@@ -1,14 +1,15 @@
+from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 
-from .. import config
-from .handlers_repository import HandlersRepository as hr
+from ..handlers_repository import HandlersRepository as hr
 
 __all__ = ["start"]
 
 
 @hr.message_handler(commands=["start"], state="*")
-async def start(message: Message):
+async def start(message: Message, state: FSMContext):
+    await message.delete()
+    await state.finish()
+
     msg = f"Hello, {message.from_user.full_name}!\nThis is RSS feeds aggregator bot.\n"
-    if config.DEBUG:
-        msg = f"{msg}Chat id is {message.chat.id}"
     await message.answer(msg)
