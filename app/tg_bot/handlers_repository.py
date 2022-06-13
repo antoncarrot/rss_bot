87cfabb1a22f3227e68196d48f7aa578c81c8a31
antoncarrot <antoncarrot@users.noreply.github.com>
2022-06-13 20:59:59 +0300

    #5 Add callback handler decorator

--- app/tg_bot/handlers_repository.py
+++ app/tg_bot/handlers_repository.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import List, Tuple
 
 from aiogram.types import BotCommand
 
@@ -6,7 +6,8 @@ __all__ = ["HandlersRepository"]
 
 
 class HandlersRepository:
-    _msg_handlers: List[Dict] = []
+    _msg_handlers: List[Tuple] = []
+    _callback_handlers: List[Tuple] = []
     _commands: List[BotCommand] = []
 
     @classmethod
@@ -14,6 +15,7 @@ class HandlersRepository:
         cls,
         callback=None,
         *,
+        filters=None,
         commands: List[str] = None,
         regexp=None,
         content_types=None,
@@ -23,15 +25,18 @@ class HandlersRepository:
         **kwargs,
     ):
         def decorator(callback):
-            conf = {
-                "callback": callback,
+            _args = [callback]
+            if filters:
+                _args += filters
+            _kwargs = {
                 "commands": commands,
                 "regexp": regexp,
                 "content_types": content_types,
                 "state": state,
                 "run_task": run_task,
             }
-            cls._msg_handlers.append(conf)
+            _kwargs.update(kwargs)
+            cls._msg_handlers.append((_args, _kwargs))
             if description:
                 for cmd in commands:
                     cls._commands.append(BotCommand(command=cmd, description=description))
@@ -42,7 +47,35 @@ class HandlersRepository:
         return decorator(callback)
 
     @classmethod
+    def callback_handler(
+        cls,
+        callback=None,
+        *,
+        filters=None,
+        state=None,
+        run_task=None,
+        **kwargs,
+    ):
+        def decorator(callback):
+            _args = [callback]
+            if filters:
+                _args += filters
+            _kwargs = {
+                "state": state,
+                "run_task": run_task,
+            }
+            _kwargs.update(kwargs)
+            cls._callback_handlers.append((_args, _kwargs))
+            return callback
+
+        if callback is None:
+            return decorator
+        return decorator(callback)
+
+    @classmethod
     async def register_handlers(cls, dp):
-        for handler_conf in cls._msg_handlers:
-            dp.register_message_handler(**handler_conf)
+        for _args, _kwargs in cls._msg_handlers:
+            dp.register_message_handler(*_args, **_kwargs)
+        for _args, _kwargs in cls._callback_handlers:
+            dp.register_callback_query_handler(*_args, **_kwargs)
         await dp.bot.set_my_commands(cls._commands)
